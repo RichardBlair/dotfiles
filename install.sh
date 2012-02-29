6558c092c8175af00371e9bd6309ab1afc14c11b
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-02-28 21:54:25 -0500

    updating install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+echo "Symlinking files"
 ln -s ~/Github/dotfiles/vimrc ~/.vimrc
 ln -s ~/Github/dotfiles/zshrc ~/.zshrc
 ln -s ~/Github/dotfiles/vim ~/.vim
+
+echo "Updating submodules"
+git submodule foreach git pull origin master --recurse-submodules
+
+echo "All done."
+
