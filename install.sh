89b96ce427fa3d53e36e2efd983f0e3e63c82730
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-03-05 20:00:49 -0500

    adding .gitconfig and renaming gvim to gvimrc

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ echo "Deleting the old files"
 rm ~/.vimrc
 rm ~/.zshrc
 rm ~/.vim
-rm ~/.gvim
+rm ~/.gvimrc
 
 echo "Symlinking files"
 ln -s ~/Github/dotfiles/vimrc ~/.vimrc
 ln -s ~/Github/dotfiles/zshrc ~/.zshrc
 ln -s ~/Github/dotfiles/vim ~/.vim
-ln -s ~/Github/dotfiles/gvim ~/.gvim
+ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
 
 echo "Updating submodules"
 git submodule foreach git pull origin master --recurse-submodules
