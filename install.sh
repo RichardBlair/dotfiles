3b6e77c1d934a781f489eebf81ab7d0374b09abc
Richard Blair <richard@blairingthoughts.com>
2012-05-29 23:02:00 -0400

    changed install to match my install prefs

--- install.sh
+++ install.sh
@@ -4,22 +4,16 @@ echo "initializing submodules"
 git submodule init
 git submodule update
 
-echo "Deleting the old files"
-rm ~/.vimrc
-rm ~/.zshrc
-rm ~/.vim
-rm ~/.gvimrc
-rm ~/.gitconfig
-rm ~/.gitignore
+echo "Backup the old files"
+mv ~/.vimrc ~/.vimrc.old
+mv ~/.vim ~/.vim.old
+mv ~/.gvimrc ~/.gvim.old
 
 
 echo "Symlinking files"
-ln -s ~/Github/dotfiles/vimrc ~/.vimrc
-ln -s ~/Github/dotfiles/zshrc ~/.zshrc
-ln -s ~/Github/dotfiles/vim ~/.vim
-ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
-ln -s ~/Github/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/Github/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/gvimrc ~/.gvimrc
 
 echo "Updating submodules"
 git submodule foreach git pull origin master --recurse-submodules
